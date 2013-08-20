0866a70b2119f90d78b820f15a6deb725d9c7cd2
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-08-20 20:39:56 -0300

    Add table variables on room booking index

--- app/controllers/room_booking_controller.rb
+++ app/controllers/room_booking_controller.rb
@@ -1,5 +1,9 @@
 class RoomBookingController < ApplicationController
   before_filter :authenticate_user!
   def index
+    @hours = ReservationHelper::HOURS
+    @days = ReservationHelper::DAYS
+  end
+
   end
 end
