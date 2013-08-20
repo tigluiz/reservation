52b9e139cbca924e6fd1cbca2beca8a226bfa823
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-08-19 23:42:08 -0300

    Add reservation controller tests

--- spec/controllers/reservation_controller_spec.rb
+++ spec/controllers/reservation_controller_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe ReservationController do
-
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  context "Correct sign_in" do
+    login_user
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        expect(response).to be_success
+      end
     end
   end
-
+  context "Incorrect sign_in" do
+    describe "GET 'index'" do
+      it "returns http fail" do
+        get 'index'
+        expect(response).to_not be_success
+      end
+    end
+  end
 end
