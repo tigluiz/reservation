b5a41b58c262957d5c71beb65751509e08aca926
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-08-20 02:10:18 -0300

    Add room booking scopes

--- app/models/room_booking.rb
+++ app/models/room_booking.rb
@@ -1,4 +1,11 @@
 class RoomBooking < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true
+  scope :active, lambda {where(active: true)}
+  scope :actual, lambda {where(reserved_at: actual_week)}
+
+  private
+    def self.actual_week
+      Time.zone.now.beginning_of_week..Time.zone.now.end_of_week
+    end
 end
