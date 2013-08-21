a86206ff82682bc4c8321195565d281eebed36d0
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-08-21 00:42:17 -0300

    Add destroy action on roombooking

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Reservation::Application.routes.draw do
   get "room_booking/index", as: "reservations"
   post "room_booking/create"
+  post "room_booking/destroy"
   devise_for :users
   root 'home#index'
 end
